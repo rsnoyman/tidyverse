109eea443c0de486da901eddae0f536b755cc752
rsnoyman <r.snoyman@unsw.edu.au>
2020-06-15 12:14:11 +1000

    improved story of examples

--- code_examples.R
+++ code_examples.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+diamonds
+
 diamonds <- rename(diamonds, colour = color)
 
 # select ------------------------------------------------------------------
@@ -8,6 +10,9 @@ select(diamonds, carat, cut, colour, clarity, price)
 select(diamonds, carat:clarity, price)
 select(diamonds, 1:4, price)
 select(diamonds, starts_with('c'), price)
+
+# Check the docs
+?select
 
 # mutate ------------------------------------------------------------------
 mutate(diamonds, ppc = price/carat)
@@ -25,60 +30,70 @@ arrange(diamonds, cut, colour)
 arrange(diamonds, desc(cut), colour)
 
 # summarise ---------------------------------------------------------------
+# This is an important one. Its how we derive insights from a dataset
 summarise(diamonds, m_carat = mean(carat))
 
 diamonds %>%
   group_by(colour) %>%
   summarise(mean_price = mean(price))
 
+# Really weird! Price increases as colour get worse?? 
+# We will explore this further. Think about what could be causing this
 
 # ggplot ------------------------------------------------------------------
-# Problems with simple plotting
+# Impact of Colour
+diamonds %>%
+  group_by(colour) %>%
+  summarise(mean_price = mean(price)) %>%
+  ggplot(aes(colour, mean_price)) +
+  geom_col()
 
+# Large spread. Low Confidence
+diamonds %>%
+  ggplot(aes(x = colour, y = price)) +
+  geom_boxplot() 
+
+# What else is impacting price?
+diamonds %>%
+  group_by(colour) %>%
+  summarise(median_carats = median(carat))
+
+# More good coloured diamonds with lower carats. Those are influencing the price
+# This tells me that carats have a much bigger impact on price than colour
+
+diamonds %>%
+  filter(carat > 0.9, carat < 1.1) %>%
+  group_by(colour) %>%
+  summarise(mean_price = mean(price)) %>%
+  ggplot(aes(colour, mean_price)) +
+  geom_col()
+
+# Another way to visulaise this
+diamonds %>%
+  filter(carat < 2.5) %>%
+  ggplot(aes(x = carat, fill = colour)) + 
+  geom_histogram(bins = 5, colour = 'black') +
+  theme_bw() + 
+  labs(title = 'Distribution of Diamonds by Carat and Colour',
+       x = 'Carats',
+       y = 'Number of Diamonds',
+       fill = 'Colour') +
+  theme(text = element_text(size = 25))
+
+# Let's look at carat vs price graph
 # Uneven distribution. Clustered at round numbers
 diamonds %>%
   slice_sample(n = 5000) %>%
   ggplot(aes(x = carat, y = price)) +
-  geom_point() +
-  geom_smooth()
+  geom_point() 
 
-diamonds %>%
-  mutate(ppc = price/carat) %>%
-  ggplot(aes(x = carat, y = ppc)) +
-  geom_smooth()
-
-ggplot(data = diamonds, mapping = aes(x = carat)) + 
-  geom_histogram(bins = 10)
-
-ggplot(data = diamonds, mapping = aes(x = cut, colour = clarity)) + 
-  geom_bar()
-
-diamonds %>%
-  mutate(clarity = reorder(clarity, desc(clarity))) %>%
-  ggplot(mapping = aes(x = cut, fill = clarity)) + 
-  geom_bar(colour = 'black') +
-  theme_bw() + 
-  labs(title = 'Distribution of Diamonds by Cut',
-       # subtitle = 'with breakdown of clarity',
-       x = 'Cut',
-       y = 'Number of Diamonds',
-       fill = 'Clarity') +
-  theme(text = element_text(size = 25))
-
-# Too many variables influencing price 
+# Most valuable before the bump
 diamonds %>%
   slice_sample(n = 5000) %>%
-  ggplot(aes(x = cut, y = price)) +
-  geom_boxplot() 
-
-# diamonds %>%
-#   filter(cut >= 'Good') %>%
-#   mutate(ppc = price/carat) %>%
-#   group_by(clarity) %>%
-#   summarise(median_ppc = median(ppc), .groups = 'drop') %>%
-#   mutate(clarity = reorder(clarity, desc(clarity))) %>%
-# ggplot(aes(clarity, median_ppc)) +
-#   geom_col()
+  mutate(ppc = price/carat) %>%
+  ggplot(aes(x = carat, y = ppc)) +
+  geom_point() +
+  geom_smooth()
 
 # Rapaport Report ---------------------------------------------------------
 carat_lower_bounds <- c(0.01, 0.04, 0.08, 0.15, 0.18, 
@@ -89,7 +104,7 @@ excellent_diamonds <- diamonds %>%
   filter(cut >= 'Very Good') %>%  # Only consider Very Good, Premium, Ideal Cut
   filter(carat >= 0.3) %>%
   mutate(ppc = price/(100*carat)) %>%
-  select(carat, price, ppc, colour = color, clarity)
+  select(carat, price, ppc, colour = colour, clarity)
 
 rapaport_report <- excellent_diamonds %>%
   mutate(size_interval = cut(carat,
